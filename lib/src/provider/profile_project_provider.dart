14e753904a70c4b9e67c1657084f6d6f9876267d
Mason Ellwood <masonellwood@gmail.com>
2020-11-10 12:09:10 -0700

    url

--- lib/src/provider/profile_project_provider.dart
+++ lib/src/provider/profile_project_provider.dart
@@ -9,14 +9,14 @@ class ProfileProjectList with ChangeNotifier {
         languages: ['Flutter', 'Dart'],
         plugins: ['cupertino_icons', 'animated_text_kit', 'carousel_slider', 'provider', 'dio'],
         description: 'Showcases ability to search based off of user input, get requests from an API, and dynamically update informaiton',
-        backgroundImage: 'assets/images/Kalimera-showcase-card.jpg',
+        backgroundImage: 'https://raw.githubusercontent.com/MasonEllwood/MasonEllwood.github.io/main/assets/assets/images/Kalimera-showcase-card.jpg',
         projectUrl: 'https://github.com/MasonEllwood/kalimera_news'
       ),
       ProfileProject(
         id: 0,
         title: 'AWS Cloud Practitioner',
         description: 'Issued Jun 2019. Expires Jun 2022. Credential ID: V8435DP1JFRQQYG9',
-        backgroundImage: 'assets/images/AWS-showcase-card.jpg',
+        backgroundImage: 'https://github.com/MasonEllwood/MasonEllwood.github.io/blob/main/assets/assets/images/AWS-showcase-card.jpg?raw=trueR',
       ),
     ];
 
